b7b2bcb40ce03ac00e6f2519b82f322efef98e72
edaagunes <edagunes00@hotmail.com>
2024-11-01 14:59:35 +0300

    Added relations and created EfDal and Services and Managers in BusinessLayer

--- SensiveProject.EntityLayer/Concrete/AppUser.cs
+++ SensiveProject.EntityLayer/Concrete/AppUser.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 
 namespace SensiveProject.EntityLayer.Concrete
 {
-    public class AppUser : IdentityUser<int>
-    {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string ImageUrl { get; set; }
+	public class AppUser : IdentityUser<int>
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string ImageUrl { get; set; }
+		public List<Article> Articles { get; set; }
+        public List<Comment> Comments { get; set; }
     }
 }
--- SensiveProject.EntityLayer/Concrete/Comment.cs
+++ SensiveProject.EntityLayer/Concrete/Comment.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace SensiveProject.EntityLayer.Concrete
 {
-    public class Comment
-    {
-        public int CommentId { get; set; }
-        public string Detail { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public bool Status { get; set; }
-    }
+	public class Comment
+	{
+		public int CommentId { get; set; }
+		public string Detail { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public bool Status { get; set; }
+		public int AppUserId { get; set; }
+		public AppUser AppUser { get; set; }
+	}
 }

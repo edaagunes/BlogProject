d42f43f6125dcbe35a739e517d162a6271e7cedb
edaagunes <edagunes00@hotmail.com>
2024-11-27 23:16:33 +0300

    Added DefaultLayout and view components for Contact Page

---
+++ SensiveProject.PresentationLayer/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensiveProject.EntityLayer.Concrete;
+
+namespace SensiveProject.PresentationLayer.Controllers
+{
+	public class ContactController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public PartialViewResult SendContactMessage()
+		{
+			return PartialView();
+		}
+		[HttpPost]
+		public IActionResult SendContactMessage(Contact contact)
+		{
+			return RedirectToAction("Index","Contact");
+		}
+	}
+}
---
+++ SensiveProject.PresentationLayer/Controllers/NewsletterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensiveProject.EntityLayer.Concrete;
+
+namespace SensiveProject.PresentationLayer.Controllers
+{
+	public class NewsletterController : Controller
+	{
+
+		[HttpGet]
+		public PartialViewResult Subscribe()
+		{
+			return PartialView();
+		}
+		[HttpPost]
+		public IActionResult Subscribe(NewsLetter newsLetter)
+		{
+			// burada gittigi sayfa degisecek
+			return RedirectToAction("Index","Category");
+		}
+	}
+}
